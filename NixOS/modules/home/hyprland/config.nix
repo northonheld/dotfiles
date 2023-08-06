98666077fe6e7e0e1b688570f44c243c9e7aa5a8
northonheld <northonheld@gmail.com>
2023-08-06 15:17:42 -0300

    remove default values for blur decoration

--- NixOS/modules/home/hyprland/config.nix
+++ NixOS/modules/home/hyprland/config.nix
@@ -75,11 +75,8 @@ in
           shadow_render_power = 3
           col.shadow = rgba(1a1a1aee)
           blur {
-            enable = true
             size = 7
             passes = 4
-            ignore_opacity = false
-            new_optimizations = true
           }
       }
 
