30e57ca82d9768ed53236b1fc1bc583df87d730c
northonheld <northonheld@gmail.com>
2023-08-06 03:07:47 -0300

    disable copySystemConfiguration

--- NixOS/modules/core/configuration.nix
+++ NixOS/modules/core/configuration.nix
@@ -113,7 +113,7 @@
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
+  #system.copySystemConfiguration = true;
   system.autoUpgrade.enable = true;
   system.autoUpgrade.allowReboot = true;
   system.autoUpgrade.channel = "https://channels.nixos.org/nixos-23.05";
