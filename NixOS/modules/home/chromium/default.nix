2fc994da654b6bf3ee6ebb76b4d4d9f5db823cf6
northonheld <northonheld@gmail.com>
2023-08-20 22:29:37 -0300

    change everything

--- NixOS/modules/home/chromium/default.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs, ... }: {
-  programs.chromium = {
-    enable = true;
-  };
-}
--- NixOS/modules/home/git/default.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }: {
-  programs.git.enable = true;
-  programs.git.userName = "northonheld";
-  programs.git.userEmail = "northonheld@gmail.com";
-  home.packages = [ pkgs.gh ];
-}
